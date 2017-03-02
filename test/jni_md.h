1e4f3b2d91b0caac3e26cf41c39c9212208850a9
Gene Hightower <gene@digilicious.com>
2017-03-01 17:39:48 -0800

    Copyright notice and license declaration

--- test/jni_md.h
+++ test/jni_md.h
@@ -1,3 +1,20 @@
+/*
+Copyright © 2016-2017 Gene Hightower <gene@digilicious.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  See the file LICENSE.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _JAVASOFT_JNI_MD_H_
 #define _JAVASOFT_JNI_MD_H_
 
